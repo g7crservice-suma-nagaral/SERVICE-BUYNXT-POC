30ab9be06212fe6169ac445be64cbecccaeaecb3
g7crservice-suma-nagaral <Suma.Nagaral@g7cr.com>
2024-12-31 14:40:28 +0530

    code

--- Buynxt_UI/assests/js/config.js
+++ Buynxt_UI/assests/js/config.js
@@ -1,5 +1,5 @@
-// const API_URL = 'http://127.0.0.1:8000/';
+// const API_URL = '';
 
-//const API_URL = 'https://g7cr-whc-poc.azurewebsites.net/';
+//const API_URL = '';
 
-const API_URL = "https://buynxtwebapp-aeata8gtaddneweb.centralindia-01.azurewebsites.net/";
+const API_URL = "";
